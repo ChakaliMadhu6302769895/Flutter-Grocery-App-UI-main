bfa3e2d0886512a983913895dbc0ed3e183babc5
ChakaliMadhu6302769895 <gauthamroyalz12324@gmail.com>
2024-01-12 13:53:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/screens/splash_screen.dart';
+import 'package:grocery_app/styles/theme.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: themeData,
+      home: SplashScreen(),
+    );
+  }
+}
+
